de312e823f9b2095fed600e4d281bf23a950dd4e
Davide210 <davide.carletti@capellinisauro.edu.it>
2025-05-10 11:28:21 +0200

    stampo 3 volte hello nome

--- HelloNome.java
+++ HelloNome.java
@@ -1,5 +1,9 @@
-public Class HelloDavide {
-    public static void main(String[] args){
+class Main {
+    public static void main(String[] args) {
         System.out.println("Hello, Davide!");
+        
+        for(int i=0; i < 3; i++){
+            System.out.println("Hello, Davide!");
+        }
     }
 }
