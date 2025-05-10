1f4c99f71a357f43a81fdf4370efe27318bc7fe1
Davide210 <davide.carletti@capellinisauro.edu.it>
2025-05-10 11:55:25 +0200

    Aggiunta stampa da vector

--- HelloNome.java
+++ HelloNome.java
@@ -2,12 +2,16 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Davide!");
         
-        for(int i=0; i < 3; i++){
-            System.out.println("Hello, Davide!");
+        Vector<Character> lettere = new Vector<>();
+        lettere.add('d');
+        lettere.add('a');
+        lettere.add('v');
+        lettere.add('i');
+        lettere.add('d');
+        lettere.add('e');
+
+        for (Character lettera : lettere){
+            System.out.println(lettera);
         }
-
-        System.out.println("11/01/2008");
-
-        System.out.println("Forza juve!");
     }
 }
